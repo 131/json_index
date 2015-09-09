1edc1c64880543802dd0fe0cdb98eb70f7414266
François Leurent <fleurent@ivsweb.com>
2015-09-09 12:47:54 +0200

    Fix regexp matching

--- test/index.js
+++ test/index.js
@@ -15,39 +15,42 @@ describe("Testing inner lib", function(){
 
       var data = JSON.parse(fs.readFileSync("test/references/data.json", 'utf-8'));
 
- 	  var Syncfiles = Index.fetchSync(dir, null, null, null)
+    var Syncfiles = Index.fetchSync(dir, null, null, null)
 
       var foo = Index.fetch(dir, null, null, null, function(err, hash){
-      	expect(hash).to.eql(data);
-      	expect(Syncfiles).to.eql(data);
+          expect(hash).to.eql(data);
+          expect(Syncfiles).to.eql(data);
 
-      	done(); 
-      	});
+          done(); 
+        });
     });
 
     it("should should compare to commited with exlude", function(){
       var dir = "test/references/data";
 
- 	  var Syncfiles = Index.fetchSync(dir, null, ["ar-tests_drawing.png"], null) ;
- 	  expect(Syncfiles).to.eql({}) ;
- 	  
+      var Syncfiles = Index.fetchSync(dir, null, ["ar-.*.png"], null) ;
+      expect(Syncfiles).to.eql({}) ;
+
+      var Syncfiles = Index.fetchSync(dir, null, [new RegExp("ar-tests_drawing.png")], null) ;
+      expect(Syncfiles).to.eql({}) ;
     });
+
 
     it("should should compare to commited with remap", function(){
       var dir = "test/references/data";
 
       var data = JSON.parse(fs.readFileSync("test/references/data.json", 'utf-8'));
 
- 	  var Syncfiles = Index.fetchSync(dir, null, null, {
- 	  	"ar-tests_drawing.png" : "image.png"
- 	  }) ;
+    var Syncfiles = Index.fetchSync(dir, null, null, {
+      "ar-tests_drawing.png" : "image.png"
+    }) ;
 
- 	  data["/image.png"] = data["/ar-tests_drawing.png"] ;
- 	  data["/image.png"]["file_path"] = "/image.png" ;
- 	  data = omit(data, "/ar-tests_drawing.png")
+    data["/image.png"] = data["/ar-tests_drawing.png"] ;
+    data["/image.png"]["file_path"] = "/image.png" ;
+    data = omit(data, "/ar-tests_drawing.png")
 
- 	  expect(Syncfiles).to.eql(data) ;
- 	  
+    expect(Syncfiles).to.eql(data) ;
+    
     });
 
 });
